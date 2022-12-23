6ac85e8e67e084261f0ae5ad58549c44b9c5e8e1
joec1368 <chenforwork1368@gmail.com>
2022-12-23 18:06:14 +0800

    add env.py

---
+++ env.py
@@ -0,0 +1,11 @@
+
+import os
+
+
+
+ngrok_url = os.getenv('ngrok_url')
+database_uri = os.getenv('database_uri')
+linebotapi = os.getenv('linebotapi')
+linehandler = os.getenv('linehandler')
+weatherToken = os.getenv('weatherToken')
+
